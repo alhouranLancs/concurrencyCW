b40ea9aea366c23dd8b66885cae2a27aced65e62
alhouranLancs <z.alhourani@lancaster.ac.uk>
2023-10-16 09:41:23 +0100

    Initial commit

---
+++ InventoryMain.java
@@ -0,0 +1,12 @@
+/**
+ * InventoryMain
+ */
+public class InventoryMain {
+
+    public static void main(String[] args) {
+        int addOp = Integer.parseInt(args[0]);
+        int subOP = Integer.parseInt(args[1]);
+        System.out.println("the number of add is " + addOp + "the number of subtract op " + subOP);
+    }
+    
+}
