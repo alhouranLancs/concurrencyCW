d4925836e25aaf36695a9d3c251201902510486f
A1Z41D <zhourani2014@gmail.com>
2023-10-22 16:57:14 +0100

    Added Threads but no Join()

--- InventoryMain.java
+++ InventoryMain.java
@@ -3,22 +3,43 @@
  */
 public class InventoryMain {
     static int inventory = 0;
+
     public static void main(String[] args) {
         int addOp = Integer.parseInt(args[0]);
         int subOP = Integer.parseInt(args[1]);
         System.out.println("the number of add is " + addOp + "the number of subtract op " + subOP);
-        
-        for(int i = 0; i>addOp; i++)
+
+        InventoryMain warehouse = new InventoryMain();
+
+        for (int i = 0; i < addOp; i++)
         {
-            add();
+            Thread addThread = new Thread(() -> {
+                warehouse.add();
+                System.out.println("An item has been added. Current inventory size: " + inventory);
+            });
+            addThread.start();
         }
+
+        for (int i = 0; i < subOP; i++) 
+        {
+            Thread subThread = new Thread(() -> {
+                warehouse.sub();
+                System.out.println("An Iitem has been removed. Current inventory size: " + inventory);
+            });
+            subThread.start();
+        }
+
+        
     }
-    public synchronized static int add(){
+
+    public synchronized int add() {
         inventory++;
         return inventory;
     }
-    public synchronized int sub(){
+
+    public synchronized int sub() {
         inventory--;
         return inventory;
     }
+
 }
