6e439123cd92ac61521a94f6cd1af5fae5afe35c
A1Z41D <zhourani2014@gmail.com>
2023-10-24 23:36:20 +0100

    Added JavaDoc and comments

--- InventoryMain.java
+++ InventoryMain.java
@@ -2,37 +2,47 @@
  * InventoryMain
  */
 public class InventoryMain {
+    
     private static int inventory = 0;
 
+    /**
+     * Main Method that starts the inventory manipulation process, by taking the number of add
+     * operations as a first parameter and the number of subtract operations as the second parameter.
+     * 
+     * @param args Command-line arguments: number of add operations and then the number of subtract operations.
+     */
     public static void main(String[] args) 
     {
+        // Variables that store the command line arguments for the number of operations
         int addOp = Integer.parseInt(args[0]);
         int subOP = Integer.parseInt(args[1]);
         System.out.println("The number of add is " + addOp + "the number of subtract op " + subOP);
 
-        InventoryMain warehouse = new InventoryMain();
-
+        // Arrays of threads that store the add and subtract operations
         Thread[] addThreads = new Thread[addOp];
         Thread[] subThreads = new Thread[subOP];
 
+        // Creating and starting threads for adding operations.
         for (int i = 0; i < addOp; i++)
         {
             addThreads[i] = new Thread(() -> {
-                warehouse.add();
+                add();
                 System.out.println("Added. Current inventory size = " + inventory);
             });
-            addThreads[i].start();
+            addThreads[i].start(); // Start the thread
         }
 
+        // Creating and starting threads for subtract operations.
         for (int i = 0; i < subOP; i++) 
         {
             subThreads[i] = new Thread(() -> {
-                warehouse.sub();
+                sub();
                 System.out.println("Removed. Current inventory size = " + inventory);
             });
-            subThreads[i].start();
+            subThreads[i].start(); // Start the thread
         }
 
+        // calling join() on the threads to ensure that they have all completed
         for (int i = 0; i < addOp; i++) 
         {
             try {
@@ -53,22 +63,29 @@ public class InventoryMain {
             }
         }
 
+        // Prints final inventory size
         System.out.println("Final inventory size = " + inventory);
 
         
-    }
+    } 
 
-    public static int getInventory() {
-        return inventory;
-    }
-
+    /**
+     * Adds an item to the inventory. This method is synchronized to ensure atomicity of threads to avoid race conditions.
+     *
+     * @return Inventory size after adding an item.
+     */
     public synchronized static int add()
     {
         inventory++;
         return inventory;
     }
 
-    public synchronized int sub()
+    /**
+     * Adds an item to the inventory. This method is synchronized to ensure atomicity of threads to avoid race conditions.
+     *
+     * @return Inventory size after removing an item.
+     */
+    public synchronized static int sub()
     {
         inventory--;
         return inventory;
